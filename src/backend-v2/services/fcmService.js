e0ff04e6e43ccbb586246ddf3584a8aca5ee7e34
malkotohuski <malkotohuski@gmail.com>
2026-03-18 17:44:58 +0200

    ttt

--- src/backend-v2/services/fcmService.js
+++ src/backend-v2/services/fcmService.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
+const path = require('path');
 
-// Взимаме JSON-а от env
+// Абсолютен път до JSON
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
 
+// Инициализация
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(require(serviceAccount)),
   });
 }
 
 const sendPush = async (fcmToken, title, body, data = {}) => {
   try {
     console.log('👉 SENDING PUSH TO:', fcmToken);
-
     const message = {
       token: fcmToken,
       notification: {title, body},
       data,
     };
-
     const response = await admin.messaging().send(message);
     console.log('✅ PUSH SUCCESS:', response);
   } catch (err) {
