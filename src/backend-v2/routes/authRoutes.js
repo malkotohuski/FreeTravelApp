b95c76fc47d7198dd049c6701b6291aaa01618bf
malkotohuski <malkotohuski@gmail.com>
2026-04-08 16:36:00 +0300

    update

--- src/backend-v2/routes/authRoutes.js
+++ src/backend-v2/routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authenticateJWT = require('../middlewares/authenticateJWT');
 
 router.post('/register', authController.register);
 router.post('/confirm', authController.confirmEmail);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
+router.post('/logout', authenticateJWT, authController.logout);
 
 // 🔄 refresh token
 router.post('/refresh', authController.refreshToken);
