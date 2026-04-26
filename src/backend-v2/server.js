cf46e999c10836b01ba580800cd908987457c3fa
malkotohuski <malkotohuski@gmail.com>
2026-04-26 11:20:45 +0300

    update

--- src/backend-v2/server.js
+++ src/backend-v2/server.js
@@ -52,7 +52,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({extended: true, limit: '10mb'}));
 app.set('trust proxy', 1);
 
 const loginLimiter = rateLimit({
